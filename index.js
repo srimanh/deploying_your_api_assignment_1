66939b803ef51610a47db6c171441c6556553b98
unknown <srimandgl2004@gmail.com>
2025-01-04 16:41:38 +0530

    Implemented dotenv and environment variables management

--- index.js
+++ index.js
@@ -1,10 +1,14 @@
 const express = require('express');
 const { resolve } = require('path');
+require('dotenv').config(); 
 
 const app = express();
-const port = 3010;
-
+const port = process.env.PORT || 3010; 
 app.use(express.static('static'));
+
+console.log('API_KEY:', process.env.API_KEY);
+console.log('SERVER_SECRET:', process.env.SERVER_SECRET);
+console.log('Is Kalvian:', process.env.IS_KALVIAN === 'true');
 
 app.get('/', (req, res) => {
   res.sendFile(resolve(__dirname, 'pages/index.html'));
